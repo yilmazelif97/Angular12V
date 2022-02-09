cf7fecdc6ed1008acd20511bf77299ce41801ac8
yilmazelif97 <elifyilmaz587@gmail.com>
2022-02-09 19:06:06 +0300

    search

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,14 +3,15 @@ import {
   Component,
   ElementRef,
   OnInit,
+  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 
 export interface Todo {
-  userId: number;
   id: number;
   title: string;
   completed: boolean;
+  userId: number;
 }
 
 @Component({
@@ -30,46 +31,70 @@ export class TodosComponent implements OnInit {
       .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
       .subscribe((res) => {
         this.data = res;
+        this.filteredData = this.data;
+
+        console.log('data', this.data);
       });
   }
 
   editMode: boolean = false;
-  editItem: Todo = {} as Todo;
+  selectedId: number = -1;
 
-  edit(id: number) {
+  edit(id: number, inpt: HTMLInputElement) {
     this.editMode = true;
     const editData = this.data.find((x) => x.id == id);
-    // as Todo tip tanımlama için kullanılıyor
-
-    this.editItem = editData || ({} as Todo); 
+    this.selectedId = editData?.id || -1;
+    inpt.value = editData?.title || '';
   }
 
   //#inpt olarak templete ref verdik
-
   update(inpt: HTMLInputElement) {
     this.editMode = false;
-    this.editItem.title = inpt.value;
-    this.editItem= {} as Todo;
+    // this.editItem.title = inpt.value;
+    // inpt.value = ''; // bug fix
+
+    let editData = this.data.find((x) => x.id == this.selectedId) as Todo;
+    editData.title = inpt.value;
+    // kendi referansını günceller
+    this.data = [...this.data];
+    this.SetFilteredTodos();
+
+    inpt.value = '';
   }
 
   add(inpt: HTMLInputElement) {
-    const todo: Todo = {title: inpt.value, userId:1, id:Math.random(), completed:false};
-    this.data = [todo, ... this.data];
-    inpt.value='';
+    const todo: Todo = {
+      title: inpt.value,
+      userId: 1,
+      id: Math.random() * 100000000,
+      completed: false,
+    };
+    this.data = [todo, ...this.data];
+    // yeni bir item ekleyip referans günceller
+    this.SetFilteredTodos();
+    inpt.value = '';
   }
 
   delete(id: number) {
     this.editMode = false;
-    const result = window.confirm("silmek istediğinize emin misiniz");
 
-    if(result){
-      this.data = [...this.data.filter(x=>x.id != id)];
+    const result = window.confirm('silmek istediğinize emin misiniz');
+
+    if (result) {
+      // silinen id çıkar referansı güncelle
+      this.data = [...this.data.filter((x) => x.id != id)];
+      this.SetFilteredTodos();
     }
   }
 
-
-  search(searchText:string){
-    const filteredData = this.data.filter(x=>x.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()));
+  search(searchText: string) {
+    console.log('searchText', searchText);
+    this.filteredData = this.data.filter((x) =>
+      x.title.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+    );
   }
 
+  private SetFilteredTodos() {
+    this.filteredData = [...this.data];
+  }
 }
